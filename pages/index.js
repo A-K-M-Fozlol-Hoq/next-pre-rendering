c099fc7580d5862fc4866acf4ab02adcedec64d2
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-28 16:40:02 +0600

    SSG with Dynamic Parameters [website crashes at posts/id route, have to ix it]

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div >
       <h1>Next js pre rendering</h1>
+      <Link href='/users'><a>Users</a></Link>
+      <Link href='/posts'><a>posts</a></Link>
     </div>
   )
 }
---
+++ pages/posts/index.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import React from 'react';
+
+const PostList = ({posts}) => {
+    return (
+        <div>
+            <h1>Post lists</h1>
+            {
+                posts.map((post) =>
+                <div key={post.id}>
+                    <Link href={`/posts/${post.id}`} passHref>
+                        <h1>{post.id} - {post.title} </h1>
+                    </Link>
+                    <hr />
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default PostList;
+
+export async function getStaticProps(props) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json();
+
+    return{
+        props:{
+            posts:data.slice(0,3)
+        }
+    }
+}
