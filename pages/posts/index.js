dbbf0195e9d13715c3598854723dc5b70e242c62
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-29 11:53:21 +0600

    fixed bug with getStaticPaths

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -20,7 +20,7 @@ const PostList = ({posts}) => {
 
 export default PostList;
 
-export async function getStaticProps(props) {
+export async function getStaticProps() {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json();
 
